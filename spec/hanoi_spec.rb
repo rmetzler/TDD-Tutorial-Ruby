a7f0c99c2779cab0ad5e81f3b35e72b8cf115605
Richard Metzler <richard@eaglefeed.me>
2012-04-10 13:17:25 +0200

    hanoi spec

--- spec/hanoi_spec.rb
+++ spec/hanoi_spec.rb
@@ -26,33 +26,38 @@ class Stack
     @items.pop
   end
   
+  def to_s
+    @items.clone.unshift('|').join('-')
+  end
+  
+  
 end
 
 describe Stack do
   
+  before(:each) do
+    @stack = Stack.new
+  end
+  
   it "is empty when created" do
-    stack = Stack.new
-    stack.should be_empty
+    @stack.should be_empty
   end
   
   it "can push and pop, and it should be the same item" do
-    stack = Stack.new
     item1 = {}
-    stack.push item1
-    item2 = stack.pop
+    @stack.push item1
+    item2 = @stack.pop
     
     item2.should equal item1
   end  
   
   it "should not pop when empty" do
-    stack = Stack.new
-    lambda {stack.pop}.should raise_error
+    lambda {@stack.pop}.should raise_error
   end
   
   it "should not push when to small" do
-    stack = Stack.new
-    stack.push 10
-    lambda {stack.push 9}.should raise_error
+    @stack.push 10
+    lambda {@stack.push 9}.should raise_error
   end
       
 end
@@ -104,49 +109,58 @@ end
 
 
 describe Hanoi do
+
+  before(:each) do
+    @hanoi = Hanoi.new
+  end
   
   it "has only discs on stack A after starting" do
-    hanoi = Hanoi.new
-    hanoi.stack(:a).should_not be_empty
-    hanoi.stack(:b).should be_empty
-    hanoi.stack(:c).should be_empty  
+    @hanoi.stack(:a).should_not be_empty
+    @hanoi.stack(:b).should be_empty
+    @hanoi.stack(:c).should be_empty  
   end
   
   it "should be able to move items" do
-    hanoi = Hanoi.new
-    hanoi.stack(:b).should be_empty
-    hanoi.move(:a, :b)
-    hanoi.stack(:b).should_not be_empty
+    @hanoi.stack(:b).should be_empty
+    @hanoi.move(:a, :b)
+    @hanoi.stack(:b).should_not be_empty
   end 
   
   it "should not allow to move a disc from an empty stack" do
-    hanoi = Hanoi.new
-    hanoi.stack(:b).should be_empty
-    lambda{hanoi.move(:b, :c)}.should raise_error
+    @hanoi.stack(:b).should be_empty
+    lambda{@hanoi.move(:b, :c)}.should raise_error
   end
   
   it "should not be able to put a bigger disk on a smaller disk" do
-    hanoi = Hanoi.new
-    hanoi.move(:a, :b)
+    @hanoi.move(:a, :b)
     #die zweite disk ist größer als die oberste 
-    lambda {hanoi.move(:a, :b)}.should raise_error
+    lambda {@hanoi.move(:a, :b)}.should raise_error
   end
 
   it "should be able to win" do
     has_won = "not yet"
-    hanoi = Hanoi.new 2 do
-      hanoi.should be_won
+    @hanoi = Hanoi.new 2 do
+      @hanoi.should be_won
       has_won = "I win"
     end
     
-    hanoi.should_not be_won
-    hanoi.move(:a, :b)
-    hanoi.should_not be_won
-    hanoi.move(:a, :c)
-    hanoi.should_not be_won
-    hanoi.move(:b, :c)
+    @hanoi.should_not be_won
+    @hanoi.move(:a, :b)
+    @hanoi.should_not be_won
+    @hanoi.move(:a, :c)
+    @hanoi.should_not be_won
+    @hanoi.move(:b, :c)
     
     has_won.should == "I win"
   end
   
 end
+
+def printHanoi( hanoi )
+  [:a, :b, :c].each do |s|
+    puts hanoi.stack(s)
+  end
+end
+
+printHanoi Hanoi.new
+
